2e0bd2f27cdf5728265ddfc45da19b8fb3b100a7
IrvinOT <irvintrejoc@gmail.com>
2023-07-18 23:39:03 -0600

    bug fix, error cuando anime to show esta vacio

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ animes_jk = scrap.get_anime_list()
 animes_notion = ntn.get_data()
 filter = Filter(animes_notion, animes_jk)
 animes_to_show = filter.get_animes_to_show()
+if len(animes_to_show)  == 0:
+    exit()
 scrap.open_anime_tabs(animes_to_show)
 ntn.update_animes(animes_to_show)
 
